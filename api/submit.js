5cdf7e724cd365e1a79e30cfe1279a9e64248660
𝐋𝐮𝐬𝐜𝐚𝐚 <lucas.martinho@viasapienslab.com.br>
2025-12-25 17:15:27 -0300

    Add GET method support to submit handler

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return res.json({ open: process.env.FORM_OPEN === "true" })
+  }
+
   if (req.method !== "POST") {
     res.status(405).end()
     return
